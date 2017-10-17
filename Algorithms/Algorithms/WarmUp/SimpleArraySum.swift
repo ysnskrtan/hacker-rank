ef0ee63c64e0a6501e6b329332650e2e26008e1b
Yasin Şükrü Tan <yasinsukrutan@Yasin-Mac.local>
2017-10-17 14:56:11 +0300

    New files added

---
+++ Algorithms/Algorithms/WarmUp/SimpleArraySum.swift
@@ -0,0 +1,25 @@
+//
+//  SimpleArraySum.swift
+//  Algorithms
+//
+//  Created by Yasin Şükrü Tan on 17.10.2017.
+//  Copyright © 2017 Yasin Şükrü Tan. All rights reserved.
+//
+
+import Foundation
+
+class SimpleArraySum {
+    
+    func test() {
+        
+        // number of elements question asks but not used below
+        let _ = Int(readLine()!)!
+        
+        // read array and map the elements to integer
+        let arr = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+        
+        // find and print the sum of array
+        let sum = arr.reduce(0, +)
+        print(sum)
+    }
+}
---
+++ Algorithms/Algorithms/WarmUp/SolveMeFirst.swift
@@ -0,0 +1,22 @@
+//
+//  SolveMeFirst.swift
+//  Algorithms
+//
+//  Created by Yasin Şükrü Tan on 17.10.2017.
+//  Copyright © 2017 Yasin Şükrü Tan. All rights reserved.
+//
+
+import Foundation
+
+class SolveMeFirst{
+    
+    func test(){
+        
+        // read integers line by line
+        let a = Int(readLine()!)!
+        let b = Int(readLine()!)!
+        
+        // print the sum
+        print(a + b)
+    }
+}
