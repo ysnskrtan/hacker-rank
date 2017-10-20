f7ad48633f756ff1b79d17a3bc71401dd969552a
Yasin Şükrü Tan <yasinsukrutan@Yasin-Mac.local>
2017-10-20 18:10:50 +0300

    Birthday Cake Candle implemented

---
+++ Algorithms/Algorithms/WarmUp/BirthdayCakeCandle.swift
@@ -0,0 +1,32 @@
+//
+//  BirthdayCakeCandle.swift
+//  Algorithms
+//
+//  Created by Yasin Şükrü Tan on 20.10.2017.
+//  Copyright © 2017 Yasin Şükrü Tan. All rights reserved.
+//
+
+import Foundation
+
+class BirthdayCankeCandle {
+    
+    func test() {
+        
+        let n = Int(readLine()!)!
+        
+        let arr = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+        
+        var numberOfCandle: Int = 0
+        
+        let maxArr = arr.max()
+        
+        for i in 0..<arr.count{
+            
+            if arr[i] == maxArr{
+                numberOfCandle += 1
+            }
+        }
+        
+        print(numberOfCandle)
+    }
+}
