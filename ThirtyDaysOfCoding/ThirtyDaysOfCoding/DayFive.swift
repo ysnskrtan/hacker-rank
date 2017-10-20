785935618aebab47b5bbc37346df3cd26d795184
Yasin Şükrü Tan <yasinsukrutan@Yasin-Mac.local>
2017-10-20 16:48:02 +0300

    Day Five implemented

---
+++ ThirtyDaysOfCoding/ThirtyDaysOfCoding/DayFive.swift
@@ -0,0 +1,22 @@
+//
+//  DayFive.swift
+//  ThirtyDaysOfCoding
+//
+//  Created by Yasin Şükrü Tan on 20.10.2017.
+//  Copyright © 2017 Yasin Şükrü Tan. All rights reserved.
+//
+
+import Foundation
+
+class DayFive{
+    
+    func test() {
+        
+        let n = Int(readLine()!)!
+        
+        for i in 1...10 {
+            
+            print("\(n) x \(i) = \(n*i)")
+        }
+    }
+}
--- ThirtyDaysOfCoding/ThirtyDaysOfCoding/main.swift
+++ ThirtyDaysOfCoding/ThirtyDaysOfCoding/main.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 // You can test code with changing the object class of test variable
-let test = DayFour()
+let test = DayFive()
 
 test.test()
