5e5c5cd9a63323a511888cd0172f492c075dda22
Yasin Şükrü Tan <yasinsukrutan@Yasin-Mac.local>
2017-10-23 01:56:54 +0300

    Day Seven implemented

---
+++ ThirtyDaysOfCoding/ThirtyDaysOfCoding/DaySeven.swift
@@ -0,0 +1,27 @@
+//
+//  DaySeven.swift
+//  ThirtyDaysOfCoding
+//
+//  Created by Yasin Şükrü Tan on 23.10.2017.
+//  Copyright © 2017 Yasin Şükrü Tan. All rights reserved.
+//
+
+import Foundation
+
+class DaySeven {
+    
+    func test() {
+        
+        // number of elements
+        let n = Int(readLine()!)!
+        
+        // read array
+        let arr = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+        
+        // iterate over the array in reverse order and print the elements separated by space
+        for i in stride(from: arr.count-1, through: 0, by: -1){
+            print(arr[i], terminator:"")
+            print(" ", terminator:"")
+        }
+    }
+}
--- ThirtyDaysOfCoding/ThirtyDaysOfCoding/main.swift
+++ ThirtyDaysOfCoding/ThirtyDaysOfCoding/main.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 // You can test code with changing the object class of test variable
-let test = DaySix()
+let test = DaySeven()
 
 test.test()
